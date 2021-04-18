5d9b98baf4a09a77bd71816be815a8e878d2b864
Vikram Grover <vik.grover71@gmail.com>
2021-04-18 19:45:37 -0400

    Add bot-button logic  in js

--- js/main.js
+++ js/main.js
@@ -10,7 +10,10 @@ $(function () {
   const $botButton = $('#bot-button');
 
   if (window.scrollY < 10) {
-    $botButton.css({ display: "block" });
+    $botButton.css({ display: "inline-block !important" });
+  }
+  else if (window.scrollY > introHeight) {
+    $topButton.css({ display: "inline-block !important" });
   }
 
   window.addEventListener(
