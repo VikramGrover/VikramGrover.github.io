54b451e93583eb99f6599e98f4e3fa25a6eeffaa
Vikram Grover <vik.grover71@gmail.com>
2021-04-20 03:41:36 -0400

    Add updated js file

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 $(function () {
-  const body = document.querySelector('body');
-  const toggle = document.getElementById('toggle');
-  const input = document.getElementById('switch');
-
   const introHeight = document.querySelector('.intro').offsetHeight;
   const topButton = document.getElementById('top-button');
   const $topButton = $('#top-button');
+  const botButton = document.getElementById('bot-button');
+  const $botButton = $('#bot-button');
+
+  if (window.pageYOffset < 10) {
+    $botButton.fadeIn(0);
+  }
+  else if (window.pageYOffset > introHeight) {
+    $topButton.fadeIn(0);
+  }
 
   window.addEventListener(
     'scroll',
@@ -15,12 +20,22 @@ $(function () {
       } else {
         $topButton.fadeOut();
       }
+
+      if (window.scrollY > 10) {
+        $botButton.fadeOut();
+      }
+      else {
+        $botButton.fadeIn();
+      }
     },
     false
   );
-
   topButton.addEventListener('click', function () {
     $('html, body').animate({ scrollTop: 0 }, 500);
+  });
+
+  botButton.addEventListener('click', function () {
+    $('html, body').animate({ scrollTop: introHeight }, 500);
   });
 
   const hand = document.querySelector('.emoji.wave-hand');
@@ -32,18 +47,35 @@ $(function () {
     }, 2000);
   }
 
+  function pointDownOnLoad() {
+    botButton.classList.add('point-down');
+    setTimeout(function () {
+      botButton.classList.remove('point-down');
+    }, 200);
+    setTimeout(function () {
+      botButton.classList.add('point-down');
+    }, 400);
+    setTimeout(function () {
+      botButton.classList.remove('point-down');
+    }, 600);
+  }
+
   setTimeout(function () {
     waveOnLoad();
   }, 1000);
 
+  setTimeout(function () {
+    if (window.pageYOffset < 10) {
+      pointDownOnLoad();
+    }
+  }, 4500);
+
   hand.addEventListener('mouseover', function () {
     hand.classList.add('wave');
   });
-
   hand.addEventListener('mouseout', function () {
     hand.classList.remove('wave');
   });
-
   window.sr = ScrollReveal({
     reset: false,
     duration: 600,
@@ -51,7 +83,6 @@ $(function () {
     scale: 1,
     viewFactor: 0.3,
   });
-
   sr.reveal('.background');
   sr.reveal('.skills');
   sr.reveal('.experience', { viewFactor: 0.2 });
